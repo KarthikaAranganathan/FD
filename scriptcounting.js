088b2777c031beebada875c333fbe1dbeae26f6e
KarthikaAranganathan <147795190+KarthikaAranganathan@users.noreply.github.com>
2023-10-21 14:20:42 +0530

    Countings

---
+++ scriptcounting.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const textInput = document.getElementById("text-input");
+  const characterCount = document.getElementById("count");
+
+  textInput.addEventListener("input", function () {
+    const text = textInput.value;
+    const count = text.length;
+    characterCount.textContent = count;
+  });
+});
